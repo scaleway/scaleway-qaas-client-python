da77bdbcd8984c43325e933cf819d96f8e585e3f
Valentin Macheret <vmacheret@scaleway.com>
2025-07-08 15:03:14 +0200

    feat(client): fix import

--- scaleway_qaas_client/__init__.py
+++ scaleway_qaas_client/__init__.py
@@ -11,38 +11,38 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-from quantum_as_a_service_api_client.models import (
+from .quantum_as_a_service_api_client.models import (
     ScalewayQaasV1Alpha1Platform as QaaSPlatform,
 )
-from quantum_as_a_service_api_client.models import (
+from .quantum_as_a_service_api_client.models import (
     ScalewayQaasV1Alpha1PlatformAvailability as QaaSPlatformAvailability,
 )
-from quantum_as_a_service_api_client.models import (
+from .quantum_as_a_service_api_client.models import (
     ScalewayQaasV1Alpha1PlatformTechnology as QaaSPlatformTechnology,
 )
-from quantum_as_a_service_api_client.models import ScalewayQaasV1Alpha1Job as QaaSJob
-from quantum_as_a_service_api_client.models import (
+from .quantum_as_a_service_api_client.models import ScalewayQaasV1Alpha1Job as QaaSJob
+from .quantum_as_a_service_api_client.models import (
     ScalewayQaasV1Alpha1JobResult as QaaSJobResut,
 )
-from quantum_as_a_service_api_client.models import (
+from .quantum_as_a_service_api_client.models import (
     ScalewayQaasV1Alpha1JobStatus as QaaSJobStatus,
 )
-from quantum_as_a_service_api_client.models import (
+from .quantum_as_a_service_api_client.models import (
     ScalewayQaasV1Alpha1Session as QaaSSession,
 )
-from quantum_as_a_service_api_client.models import (
+from .quantum_as_a_service_api_client.models import (
     ScalewayQaasV1Alpha1SessionStatus as QaaSSessionStatus,
 )
-from quantum_as_a_service_api_client.models import (
+from .quantum_as_a_service_api_client.models import (
     ScalewayQaasV1Alpha1Application as QaaSApplication,
 )
-from quantum_as_a_service_api_client.models import (
+from .quantum_as_a_service_api_client.models import (
     ScalewayQaasV1Alpha1Process as QaaSProcess,
 )
-from quantum_as_a_service_api_client.models import (
+from .quantum_as_a_service_api_client.models import (
     ScalewayQaasV1Alpha1ProcessResult as QaaSProcessResult,
 )
-from quantum_as_a_service_api_client.models import (
+from .quantum_as_a_service_api_client.models import (
     ScalewayQaasV1Alpha1ProcessStatus as QaaSProcessStatus,
 )
 from job_payload.models import (
--- setup.py
+++ setup.py
@@ -22,7 +22,7 @@ requirements = [r.strip() for r in requirements]
 
 setup(
     name="scaleway_qaas_client",
-    version="0.1.0",
+    version="0.1.1",
     project_urls={
         "Documentation": "https://www.scaleway.com/en/quantum-as-a-service/",
         "Source": "https://github.com/scaleway/scaleway-qaas-client-pythom",
