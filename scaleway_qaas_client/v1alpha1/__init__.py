81e2387f5917eff09e7b3ab6f8f2e674ae3a0025
Valentin Macheret <vmacheret@scaleway.com>
2025-08-01 18:09:57 +0200

    feat(ver): add v1alpha1 version

---
+++ scaleway_qaas_client/v1alpha1/__init__.py
@@ -0,0 +1,62 @@
+# Copyright 2025 Scaleway
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     https://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+from .client import QaaSClient
+from .job_payload.models import (
+    QaaSCircuitData,
+    QaaSCircuitSerializationFormat,
+    QaaSJobBackendData,
+    QaaSJobClientData,
+    QaaSJobData,
+    QaaSJobRunData,
+)
+from .quantum_as_a_service_api_client.models import (
+    ScalewayQaasV1Alpha1Application as QaaSApplication,
+)
+from .quantum_as_a_service_api_client.models import ScalewayQaasV1Alpha1Job as QaaSJob
+from .quantum_as_a_service_api_client.models import (
+    ScalewayQaasV1Alpha1JobResult as QaaSJobResult,
+)
+from .quantum_as_a_service_api_client.models import (
+    ScalewayQaasV1Alpha1JobResult as QaaSJobResut,
+)
+from .quantum_as_a_service_api_client.models import (
+    ScalewayQaasV1Alpha1JobStatus as QaaSJobStatus,
+)
+from .quantum_as_a_service_api_client.models import (
+    ScalewayQaasV1Alpha1Platform as QaaSPlatform,
+)
+from .quantum_as_a_service_api_client.models import (
+    ScalewayQaasV1Alpha1PlatformAvailability as QaaSPlatformAvailability,
+)
+from .quantum_as_a_service_api_client.models import (
+    ScalewayQaasV1Alpha1PlatformTechnology as QaaSPlatformTechnology,
+)
+from .quantum_as_a_service_api_client.models import (
+    ScalewayQaasV1Alpha1Process as QaaSProcess,
+)
+from .quantum_as_a_service_api_client.models import (
+    ScalewayQaasV1Alpha1ProcessResult as QaaSProcessResult,
+)
+from .quantum_as_a_service_api_client.models import (
+    ScalewayQaasV1Alpha1ProcessStatus as QaaSProcessStatus,
+)
+from .quantum_as_a_service_api_client.models import (
+    ScalewayQaasV1Alpha1Session as QaaSSession,
+)
+from .quantum_as_a_service_api_client.models import (
+    ScalewayQaasV1Alpha1SessionStatus as QaaSSessionStatus,
+)
+from .quantum_as_a_service_api_client.models import (
+    ScalewayQaasV1Alpha1SessionAccess as QaaSSessionAccess,
+)
